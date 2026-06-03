4e0672221581f5b98cbcedc65ad377b2a5701142
Surya-ux-max <surprakas14@gmail.com>
2026-06-03 20:18:34 +0530

    Refactor App component to use variables for name and designation

--- Home/src/App.jsx
+++ Home/src/App.jsx
@@ -1,10 +1,13 @@
 function App(){
+  const name = "Suryaprakash S";
+  const designation = "AI Engineer";
   return (
     <div>
-      <h1>Suryaprakash S</h1>
-      <p>AI Engineer</p>
+      <h1>{name}</h1>
+    <p>{designation}</p>
     </div>
   );
 }
 export default App;
 
+// dont forget to add the div tag to wrap the h1 and p tags, otherwise it will throw an error because a component can only return one element.
