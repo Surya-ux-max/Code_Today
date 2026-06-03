d3279a830cbcfbc7a46022959b70852478c16154
Surya-ux-max <surprakas14@gmail.com>
2026-06-03 20:22:25 +0530

    function in react

--- Home/src/App.jsx
+++ Home/src/App.jsx
@@ -1,13 +1,16 @@
+
+
+
 function App(){
-  const name = "Suryaprakash S";
-  const designation = "AI Engineer";
-  return (
+  const greet = () => {
+    return "Hello Suryaprakash"}; // Since javascript is used in the printing of the string, we need to use the return statement to return the string from the function. Otherwise, it will not work and will throw an error.
+  return(
     <div>
-      <h1>{name}</h1>
-    <p>{designation}</p>
+      <h1>{greet()}</h1>
     </div>
   );
 }
+// dont forget to add the div tag to wrap the h1 and p tags, otherwise it will throw an error because a component can only return one element.
 export default App;
 
-// dont forget to add the div tag to wrap the h1 and p tags, otherwise it will throw an error because a component can only return one element.
+
